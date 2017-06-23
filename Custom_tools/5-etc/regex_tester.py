6f802dc847041667a9dd1c075888b54e7550cfd4
initialds <initia_ds@yahoo.com>
2017-06-24 04:03:24 +0700

    add timer to regex tester

--- Custom_tools/5-etc/regex_tester.py
+++ Custom_tools/5-etc/regex_tester.py
@@ -1,14 +1,15 @@
 from __future__ import division
 import sys
 import cPickle
-import re 
+import re
+import time
 
 if __name__ == '__main__':
 
 	if(len(sys.argv) < 2):
 		print 'parameters: <file to check>'
 		exit()
-		
+
 	#load signature
 	file = open('signature.txt')
 	regex = file.read()
@@ -22,10 +23,16 @@ if __name__ == '__main__':
 	#check traffic
 	success = 0
 	failed = 0
+
+	#time traffic check
+	t0 = time.time()
 	for traffic in traffics:
 		if(re.search(regex,traffic,re.M|re.I)):
 			success += 1
 		else:
 			failed += 1
+	t1 = time.time()
+
 	print 'success = %f%%' % (success/(success+failed) * 100)
 	print 'failed = %d samples' % failed
+	print 'elapsed time = %f s' % (t1-t0)
